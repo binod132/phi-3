dc5a30d0f33af650790cb840f7c1d58755b7c6e9
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-08-26 14:51:13 +0545

    fixed

--- job.py
+++ job.py
@@ -12,7 +12,7 @@ service_account="vertex-ai@brave-smile-424210-m0.iam.gserviceaccount.com"
 # Create and submit a Custom Training Job
 job = aiplatform.CustomJob.from_local_script(
     display_name="phi-3-mini-4k-training",
-    script_path="gpu-test.py",  # The script you already embedded in the Docker image
+    script_path="train.py",  # The script you already embedded in the Docker image
     container_uri="us-docker.pkg.dev/brave-smile-424210-m0/train/phi-3:main",  # Your Docker image
     replica_count=1,  # Number of machines to run the job on
     machine_type="n1-standard-4",  # The machine type
